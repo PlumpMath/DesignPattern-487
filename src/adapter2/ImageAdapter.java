2574604d6e1296a1ebe1b16eea213d922da63e2e
yeuembubu <quanghuydang1902@gmail.com>
2015-02-01 23:58:07 +0700

    commit

--- src/adapter2/ImageAdapter.java
+++ src/adapter2/ImageAdapter.java
@@ -1,13 +1,13 @@
 package adapter2;
 
-public class Photoshop implements DrawImage {
+public class ImageAdapter implements DrawImage {
 
 	@Override
 	public void createImage() {
 		// TODO Auto-generated method stub
 		
 		// Phương thức này sẽ sử dụng phương thức của lớp Paint
-		// để khởi tạo tiêu đề của một bức ảnh 
+		// để khởi tạo tiêu đ�? của một bức ảnh 
 		
 		Drawable drawImage = new Paint();
 		String imageTitle = drawImage.draw("");
