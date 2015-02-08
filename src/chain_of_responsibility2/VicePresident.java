a62ebbf727173cb9fbf76c7e17ca80d859588e3b
yeuembubu <quanghuydang1902@gmail.com>
2015-02-08 13:23:52 +0700

    commit

--- src/chain_of_responsibility2/VicePresident.java
+++ src/chain_of_responsibility2/VicePresident.java
@@ -1,5 +1,28 @@
 package chain_of_responsibility2;
 
-public class VicePresident {
+public class VicePresident extends PRHandler {
+	
+	private static double LIMIT = 200000;
+
+	public VicePresident(String handlerName) {
+		super(handlerName);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public boolean authorized(PurchaseRequest request) {
+		// TODO Auto-generated method stub
+
+		double mount = request.getAmount();
+
+		if (mount <= LIMIT) {
+			System.out.println("Vice president " + getHandlerName()
+					+ "has authorized the PR - " + request);
+			return true;
+		} else {
+			return getNextHandler().authorized(request);
+		}
+
+	}
 
 }
